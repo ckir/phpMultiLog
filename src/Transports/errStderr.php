2b31046d955290f130d96bcddf22953578f9ad74
ckir <ckir1508@hotmal.com>
2015-01-11 15:18:16 +0200

    Initial commit

---
+++ src/Transports/errStderr.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace phpMultiLog\Transports;
+
+class errStderr {
+	
+	/**
+	 * @param array $transportParameters Transport initialization parameters
+	 */
+	public function __construct($transportParameters = array()) {
+	} // function __construct
+	
+	/**
+	 * Writes a record to stderr
+	 *
+	 * @param string $appID        	
+	 * @param string $date        	
+	 * @param integer $errno        	
+	 * @param string $errstr        	
+	 * @param string $errfile        	
+	 * @param string $errline        	
+	 * @param string $errcontext        	
+	 */
+	public function log($appID, $date, $errno, $errstr, $errfile, $errline, $errcontext) {
+		$logrecord = sprintf ( "App: %s - date: %s - errno: %s - errstr: %s - errfile: %s - errline: %s", $appID, $date, $errno, $errstr, $errfile, $errline ) . PHP_EOL;
+		$stderr = fopen ( 'php://stderr', 'w' );
+		if (! fwrite ( $stderr, $logrecord )) {
+			error_log ( $logrecord );
+		}
+		fclose ( $stderr );
+	} // function log
+} // class errStderr
